65ff6c463b2874df1b128fa51e4f9258dd92389b
Infarh <shmachilin@gmail.com>
2024-02-21 11:15:52 +0300

    Упрощение процесса добавления листов

--- MathCore.OpenXML/ExcelProcessing/Extensions/ExcelSpreadsheetDocumentEx.cs
+++ MathCore.OpenXML/ExcelProcessing/Extensions/ExcelSpreadsheetDocumentEx.cs
@@ -1,4 +1,7 @@
-﻿using DocumentFormat.OpenXml.Packaging;
+﻿using System.Linq;
+
+using DocumentFormat.OpenXml.Office2016.Excel;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace MathCore.OpenXML.ExcelProcessing.Extensions;
@@ -52,5 +55,29 @@ public static class ExcelSpreadsheetDocumentEx
         });
 
         return document;
+    }
+
+    public static (WorksheetPart Part, SheetData Rows) CreateSheet(this SpreadsheetDocument document, string SheetName)
+    {
+        if(document.WorkbookPart is not { } workbook_part)
+        {
+            workbook_part = document.AddWorkbookPart();
+            workbook_part.Workbook = new();
+        }
+
+        var sheet_part = workbook_part.AddNewPart<WorksheetPart>();
+        var sheet_data = new SheetData();
+        sheet_part.Worksheet = new(sheet_data);
+
+        var sheets = workbook_part.Workbook.Sheets ?? workbook_part.Workbook.AppendChild(new Sheets());
+        var sheet_id = sheets.ChildElements.OfType<Sheet>().Select(s => s.SheetId ?? 0).DefaultIfEmpty().Max() + 1;
+        sheets.AppendChild(new Sheet
+        {
+            Id = workbook_part.GetIdOfPart(sheet_part),
+            SheetId = sheet_id,
+            Name = SheetName ?? $"List {sheet_id}"
+        });
+
+        return (sheet_part, sheet_data);
     }
 }
