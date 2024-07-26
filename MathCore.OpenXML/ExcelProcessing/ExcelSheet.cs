fd811419b7e4a2650d3a77b65dc086a5ac234b69
Infarh <shmachilin@yandex.ru>
2024-07-26 13:43:55 +0300

    v0.0.6 - Изменён механизм заполнения полей шаблона Word

--- MathCore.OpenXML/ExcelProcessing/ExcelSheet.cs
+++ MathCore.OpenXML/ExcelProcessing/ExcelSheet.cs
@@ -33,7 +33,7 @@ public class ExcelSheet : IEnumerable<ExcelRow>
 
         var sheet_info = workbook.Workbook.Sheets
            .Cast<Sheet>()
-           .First(s => s.Name.Value == _SheetName);
+           .First(s => s.Name?.Value == _SheetName);
 
         var sheet = workbook.GetPartById(sheet_info.Id);
         var reader = new OpenXmlPartReader(sheet);
