5059473c648066eb291814f8e8e9ab3933dda6ed
Павел <shmachilin@gmail.com>
2026-06-01 13:23:51 +0300

    Рефакторинг методов расширения для класса Run и добавление нового класса ExtensionsRunProperties

--- MathCore.OpenXML/WordProcessing/Extensions/Word/ExtensionsRun.cs
+++ MathCore.OpenXML/WordProcessing/Extensions/Word/ExtensionsRun.cs
@@ -4,100 +4,119 @@ namespace MathCore.OpenXML.WordProcessing.Extensions.Word;
 
 public static class ExtensionsRun
 {
-    public static Run SetProperties(this Run run, RunProperties properties)
+    extension(Run)
     {
-        run.RunProperties = properties;
-        return run;
+        public static Run WithText(string Text) => new(new Text(Text));
+
+        public static Run WithText(string Text, string StyleId) => new(
+            new RunProperties(new RunStyle { Val = StyleId }),
+            new Text(Text));
     }
 
-    public static Run Bold(this Run run, bool IsBold = true)
+    extension(Run run)
     {
-        var properties = run.RunProperties ??= new();
-        properties.Bold = IsBold ? new() : null;
-        return run;
-    }
-
-    public static Run Italic(this Run run, bool IsItalic = true)
-    {
-        var properties = run.RunProperties ??= new();
-        properties.Italic = IsItalic ? new() : null;
-        return run;
-    }
-
-    public static Run Underline(this Run run, bool IsUnderline = true)
-    {
-        var properties = run.RunProperties ??= new();
-        properties.Underline = IsUnderline ? new() : null;
-        return run;
-    }
-
-    public static Run Color(this Run run, string Color)
-    {
-        var properties = run.RunProperties ??= new();
-        var color = properties.Color ??= new();
-        color.Val = Color;
-        return run;
-    }
-
-    public static Run FontSize(this Run run, int Size)
-    {
-        var properties = run.RunProperties ??= new();
-        var font_size = properties.FontSize ??= new();
-        font_size.Val = Size.ToString();
-        return run;
-    }
-
-    public static Run Font(this Run run, string FontName)
-    {
-        var properties = run.RunProperties ??= new();
-        var run_fonts = properties.RunFonts ??= new();
-        run_fonts.Ascii = FontName;
-        run_fonts.HighAnsi = FontName;
-        return run;
-    }
-
-    /// <summary>Разрежение символов</summary>
-    public static Run Spacing(this Run run, int Size)
-    {
-        var properties = run.RunProperties ??= new();
-        var spacing = properties.Spacing ??= new();
-        spacing.Val = Size;
-        return run;
-    }
-
-    public static Run Language(this Run run, string Language)
-    {
-        var properties = run.RunProperties ??= new();
-        if (string.IsNullOrEmpty(Language))
-            properties.RemoveAllChildren<Languages>();
-        else
+        public Run SetProperties(RunProperties properties)
         {
-            var languages = properties.Languages ??= new();
-            languages.Val = Language;
+            run.RunProperties = properties;
+            return run;
         }
 
-        return run;
-    }
+        public Run Bold(bool IsBold = true)
+        {
+            var properties = run.RunProperties ??= new();
+            properties.Bold = IsBold ? new() : null;
+            return run;
+        }
 
-    public static Run AppendText(this Run run, string str)
-    {
-        run.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Text { Text = str });
-        return run;
-    }
+        public Run Italic(bool IsItalic = true)
+        {
+            var properties = run.RunProperties ??= new();
+            properties.Italic = IsItalic ? new() : null;
+            return run;
+        }
 
-    public static Run Text(this Run run, string str)
-    {
-        if (run.GetFirstChild<DocumentFormat.OpenXml.Wordprocessing.Text>() is { } text)
-            text.Text = str;
-        else
-            run.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Text(str));
+        public Run Underline(bool IsUnderline = true)
+        {
+            var properties = run.RunProperties ??= new();
+            properties.Underline = IsUnderline ? new() : null;
+            return run;
+        }
 
-        return run;
-    }
+        public Run Color(string Color)
+        {
+            var properties = run.RunProperties ??= new();
+            var color = properties.Color ??= new();
+            color.Val = Color;
+            return run;
+        }
 
-    public static Run Tab(this Run run)
-    {
-        run.AppendChild(new TabChar());
-        return run;
+        public Run FontSize(int Size)
+        {
+            var properties = run.RunProperties ??= new();
+            var font_size = properties.FontSize ??= new();
+            font_size.Val = Size.ToString();
+            return run;
+        }
+
+        public Run Font(string FontName)
+        {
+            var properties = run.RunProperties ??= new();
+            var run_fonts = properties.RunFonts ??= new();
+            run_fonts.Ascii = FontName;
+            run_fonts.HighAnsi = FontName;
+            return run;
+        }
+
+        /// <summary>Разрежение символов</summary>
+        public Run Spacing(int Size)
+        {
+            var properties = run.RunProperties ??= new();
+            var spacing = properties.Spacing ??= new();
+            spacing.Val = Size;
+            return run;
+        }
+
+        public Run Language(string Language)
+        {
+            var properties = run.RunProperties ??= new();
+            if (string.IsNullOrEmpty(Language))
+                properties.RemoveAllChildren<Languages>();
+            else
+            {
+                var languages = properties.Languages ??= new();
+                languages.Val = Language;
+            }
+
+            return run;
+        }
+
+        public Run AppendText(string str)
+        {
+            run.AppendChild(new Text { Text = str });
+            return run;
+        }
+
+        public Run Text(string str)
+        {
+            if (run.GetFirstChild<Text>() is { } text)
+                text.Text = str;
+            else
+                run.AppendChild(new Text(str));
+
+            return run;
+        }
+
+        public Run Tab()
+        {
+            run.AppendChild(new TabChar());
+            return run;
+        }
+
+        public Run Style(string StyleId)
+        {
+            var properties = run.RunProperties ??= new();
+            properties.RunStyle = new RunStyle { Val = StyleId };
+            return run;
+        }
     }
 }
---
+++ MathCore.OpenXML/WordProcessing/Extensions/Word/ExtensionsRunProperties.cs
@@ -0,0 +1,20 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace MathCore.OpenXML.WordProcessing.Extensions.Word;
+
+public static class ExtensionsRunProperties
+{
+    extension(RunProperties)
+    {
+        public static RunProperties withStyle(string StyleId) => new(new RunStyle { Val = StyleId });
+    }
+
+    extension(RunProperties properties)
+    {
+        public RunProperties Style(string StyleId)
+        {
+            properties.RunStyle = new RunStyle { Val = StyleId };
+            return properties;
+        }
+    }
+}
